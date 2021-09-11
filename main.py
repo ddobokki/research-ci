d758292cc672419457fa27088ea7e73d6f247201
ddobokki <ddobokki@naver.com>
2021-09-12 02:30:33 +0900

    prama

--- main.py
+++ main.py
@@ -4,9 +4,5 @@ def helloworld(a: str):
     return msg
 
 
-<<<<<<< HEAD
-if __name__ == "__main__":  # pragma no cover
-=======
-if __name__ == "__main__":
->>>>>>> e23aa4d2c8a6d52d5c6d4990e45c8fd066f870bf
+if __name__ == "__main__":  # pragma: no cover
     helloworld("ddobokki")
